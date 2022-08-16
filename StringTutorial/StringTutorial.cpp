1a3f154cdc3a1f04161ab7ab7d4e8c313110836a
kksmash123 <kksmashes@gmail.com>
2022-08-16 19:29:28 +0530

    reading  string done with getline

--- StringTutorial/StringTutorial.cpp
+++ StringTutorial/StringTutorial.cpp
@@ -1,8 +1,50 @@
-
 #include <iostream>
+#include<string>
+
+using namespace std;
+
+void stringReading()
+{
+    string strl;
+    cout << "Enter a string " << endl;
+    getline(cin, strl);
+    cout << "Output of getline with string datatype" << endl;
+    cout << strl;
+
+}
+
+void charArrayReading()
+{
+    char str2[100];
+    //getline(cin, str2); -> wrong method
+    //reading char[] with getline
+    cout << "Enter a string with space" << endl;
+    cin.getline(str2, sizeof(str2));
+    cout << "Output of getline with char[] datatype" << endl;
+    cout << str2;
+
+}
+
+
+void strlengthCal(char* s)
+{
+    cout << strlen(s) << endl;;
+    int i;
+    for (i = 0;s[i] != NULL;i++);
+    cout << " i value -> string length ->>  " << i << endl;
+   
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //char s[] = "keerthu";
+    //strlengthCal(s);
+
+    //charArrayReading();
+
+   // stringReading();
+
+
 }
 
